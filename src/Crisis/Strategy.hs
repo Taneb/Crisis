040306277f959153e74122b749b0d828d829bc00
Nathan van Doorn <nvd1234@gmail.com>
2015-02-19 15:14:31 +0000

    Removed array stuff and ST from chooseMove Also increased weighting for scorePart1

--- src/Crisis/Strategy.hs
+++ src/Crisis/Strategy.hs
@@ -1,11 +1,9 @@
 module Crisis.Strategy (chooseMove, setOutcome) where
 
 import Control.Monad
-import Control.Monad.ST
-import Data.Array.IArray
-import Data.Array.ST
 import Data.Bits
 import Data.Foldable (foldMap)
+import Data.List (foldl')
 import Data.Tuple
 import Data.Vector.Unboxed (Vector)
 import qualified Data.Vector.Unboxed as V
@@ -29,48 +27,42 @@ isInShipList ship shipList = testBit shipList (fromEnum ship)
 
 chooseMove :: Board -> Vector (Board, Int) -> Board -> Maybe (Int, Int)
 chooseMove misses universes emptycells =
-  let finalScores = runSTUArray $ do
-        r <- newArray (0,107) 0 :: ST s (STUArray s Int Int)
-        V.forM_ universes $ scoreUniverse r
-        return r
+  let finalScores = V.foldl' (\r n -> V.zipWith (+) r $ scoreUniverse n) (V.replicate 108 0) universes
       (bestscore, bests) =
-        foldr (\(i, this) (score, bs) ->
+        V.foldr (\(i, this) (score, bs) ->
                 case this `compare` score of
                   LT -> (score, bs)
                   EQ -> (score, i : bs)
-                  GT -> (this, [i])) (0, []) $ assocs finalScores
+                  GT -> (this, [i])) (0, []) $ V.indexed finalScores
       coord = head bests -- TODO: random
   in if bestscore == 0 then Nothing
      else  Just $ swap $ if coord < 36
                          then coord        `divMod` 6
                          else (coord + 36) `divMod` 12
   where
-    -- might be a thought to not use ST at all?
-    scoreUniverse finalScores (universe, remShips) = do
-      scorePart1
-      scorePart2
+    scoreUniverse (universe, remShips) = V.zipWith (+) scorePart1 scorePart2
       where
         -- handy synonym for the number of remaining ships in the universe
         noRemShips = popCount remShips
 
         -- adjust scores for cells the universe believes to be DEFINITELY
         -- occupied.
-        scorePart1 = forM_ [x | x <- oiot, testBit emptycells x] $ \i ->
-          when (testBit universe i) $ do
-            x <- readArray finalScores i
-            writeArray finalScores i (x + 600 - 100*noRemShips)
+        scorePart1 = V.generate 108 $ \i ->
+          if testBit emptycells i && testBit universe i
+          then 6000 - 1000*noRemShips
+          else 0
 
         -- adjust scores for cells the universe belieces to be POSSIBLY
         -- occupied, by considering where it is possible to place a ship that
         -- hasn't already been placed.
-        scorePart2 =
-          forM_ [i|i<-[Destroyer .. Carrier],i`isInShipList`remShips] $ \ship ->
-          V.forM_ (allPositionsOf ship) $ \placement ->
-          when (placement.&.(misses.|.complement emptycells.|.universe) == Board 0 0) $
-          forM_ oiot $ \i ->
-          when (testBit placement i) $ do
-            x <- readArray finalScores i
-            writeArray finalScores i (x + 1 + noRemShips)
+        --
+        -- TODO: further vectorize, parrelellize?
+        scorePart2 = foldl' (V.zipWith (+)) (V.replicate 108 0) $ do
+          ship <- [Destroyer .. Carrier]
+          guard $ ship `isInShipList` remShips
+          placement <- V.toList $ allPositionsOf ship
+          guard $ placement .&. (misses .|. complement emptycells .|. universe) == Board 0 0
+          return $ V.generate 108 $ \i -> if testBit placement i then 1 + noRemShips else 0
 
 setOutcome :: Board -> Vector (Board, Int) -> Board ->
               (Int, Int) -> Bool -> 
